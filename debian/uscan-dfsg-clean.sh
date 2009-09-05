00d51c5bde458277f1865f55d5508d7b791e9c69
Johann Felix Soden <johfel@gmx.de>
2009-09-05 15:14:40 +0200

    Import patches-unapplied version 1.41+dfsg-1 to debian/sid
    Imported using git-ubuntu import.
    
    Changelog parent: 6f486aad9b34914117a6229d7bda501453b58c53
    
    New changelog entries:
      * Adopted the package with agreement of the previous maintainer
        - Thanks for your work Aurélien GÉRÔME!
      * DFSG-repack and use extern libitext-java (Closes: #519466)
      * Support newer gcj (Closes: #470215, #514561, #534080, #533795, #538252)
      * Handle utf8 filenames (Closes: #258377, #504014, #504128)
      * Handle utf8-data, CreationDate and ModDate fields in update_info correctly
        (Closes: #424920, #461169)
      * Rework build system
        - minimize dh rules
        - update dependencies
      * Bump Standards-Version to 3.8.3.
      * Update debian/watch and use created debian/uscan-dfsg-clean.sh
      * Clean debian/copyright from itext, use machine-readable format
        - upstream uses GPL-2+ (not unversioned GPL)
      * Add patch: use mkstemp instead of tmpnam
      * Add upstream changelog
      * Remove meaningless "useful" from short description and author-field from
        long description

---
+++ debian/uscan-dfsg-clean.sh
@@ -0,0 +1,29 @@
+#!/bin/sh 
+
+set -e
+
+#command --upstream-version version filename
+
+[ $# -eq 3 ] || exit 255
+
+echo
+
+version="$2"
+filename="$3"
+dfsgfilename=`echo $3 | sed 's,\.orig\.,+dfsg.orig.,'`
+
+tar xfz ${filename} 
+
+dir=`tar tfz ${filename} | head -1 | sed 's,/.*,,g'`
+rm -f ${filename}
+
+rm -rf ${dir}/java_libs
+rm -rf ${dir}/debian
+mv ${dir} ${dir}+dfsg.orig
+
+tar cf - ${dir}+dfsg.orig | gzip -9 > ${dfsgfilename}
+
+rm -rf ${dir}+dfsg.orig
+
+echo "${dfsgfilename} created."
+
